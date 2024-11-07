39609557a3af47ab8b9f9ad7ea0bfe99520b4b06
xatilore <90924941+xatilore@users.noreply.github.com>
2024-11-08 00:09:45 +0100

    overhaul 1.4.0

--- assets/js/global.js
+++ assets/js/global.js
@@ -8,3 +8,41 @@ window.addEventListener('scroll', function() {
         animationPaused = true; // Prevent the event from triggering multiple times
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Array of image files for Apeirophobia and Doors
+    const imageFilesA = [
+      "Apeirophobia.png",
+      //"Apeirophobia2.png",
+      //"Apeirophobia3.png",
+      //"Apeirophobia4.png",
+      //"Apeirophobia5.png",
+      //"Apeirophobia6.png",
+      //"Apeirophobia7.png",
+    ];
+  
+    const imageFilesD = [
+      "Doors1.png",
+      "Doors2.png",
+      "Doors3.png",
+      "Doors4.png",
+      "Doors5.png",
+      "Doors6.png",
+      "Doors7.png",
+    ];
+  
+    // Function to pick a random image and set it to the respective img elements
+    function pickRandomImage() {
+      // Randomly pick an image from each array
+      const randomImageA = imageFilesA[Math.floor(Math.random() * imageFilesA.length)];
+      const randomImageD = imageFilesD[Math.floor(Math.random() * imageFilesD.length)];
+      
+      // Set the src of the image elements
+      document.getElementById("apeirophobiaImage").src = "assets/img/showcase/apeirophobia/" + randomImageA;
+      document.getElementById("doorsImage").src = "assets/img/showcase/doors/" + randomImageD;
+    }
+  
+    // Pick a random image when the page loads
+    pickRandomImage();
+  });
+  
